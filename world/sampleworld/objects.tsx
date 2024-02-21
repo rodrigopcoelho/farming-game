59b42f6474718a4221e1b04b1f109f4c5a50c227
rodrigocoelho <rodrigop.coelhoo@gmail.com>
2024-02-21 17:29:40 +0000

    TILES

---
+++ world/sampleworld/objects.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="objects" tilewidth="128" tileheight="256" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="../../../../art/full version/tiles/pinetree_shake_winter.gif"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="../../../../art/full version/tiles/pinetree_shake_spring.gif"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="../../../../art/full version/tiles/pinetree_shake_fall.gif"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="../../../../art/full version/tiles/tree_shake_spring.gif"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="../../../../art/full version/tiles/tree_shake_summer.gif"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="256" source="../../../../art/full version/tiles/tree_shake.png"/>
+ </tile>
+</tileset>
